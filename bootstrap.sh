12dfb89aa2dd6d9b48b4c6ec4e54bf0a622bf79e
till <till@php.net>
2018-07-24 12:51:56 +0200

    Update: switch to localkube
     - switched to localkube (kubeadm hangggggssss)
     - moved all RBAC switches into an option flag

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,16 +1,39 @@
 #!/usr/bin/env bash
 
+#set -x
+
+ENABLE_RBAC=false
+ENABLE_INGRESS=false
+ENABLE_DEBUG=false
+
+while getopts ":r:i:h" opt; do
+  case $opt in
+    r)
+      ENABLE_RBAC=true
+      ;;
+    i)
+      ENABLE_INGRESS=true
+      ;;
+    h)
+      echo "Run with the following (optional flags): ./bootstrap -i -r"
+      echo " -i - Enables ingress"
+      echo " -r - Enables RBAC"
+      exit
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
 minikube config set memory 8192
 
-minikube start \
---vm-driver=hyperkit \
+minikube start --vm-driver=hyperkit --loglevel 0 \
+--bootstrapper=localkube \
 --logtostderr \
---loglevel 0 \
---stderrthreshold 0 \
---cache-images \
---bootstrapper=kubeadm \
---extra-config=apiserver.authorization-mode=RBAC \
---iso-url=./minikube-v0.28.0.iso
+--stderrthreshold 0
+#--cache-images
+#--iso-url=./minikube-v0.28.0.iso
 
 echo "Minikube started"
 
@@ -20,10 +43,23 @@ if [ $ingress_enabled -gt 0 ]; then
   echo "Disabled ingress"
 fi
 
-echo "Setup helm with RBAC"
+if [ "$ENABLE_INGRESS" = true ];
+then
+  minikube addons disable ingress
+fi
 
-kubectl create -f ./rbac-config.yaml
+if [ "$ENABLE_RBAC" = true ];
+then
+  echo "Setup helm with RBAC"
+  kubectl create -f ./rbac-config.yaml
+  helm init --upgrade --service-account tiller
+fi
 
-helm init --upgrade --service-account tiller
+if [ "$ENABLE_RBAC" = false ];
+then
+  helm init --upgrade
+fi
+
+
 
 echo "helm configured/initialized"
